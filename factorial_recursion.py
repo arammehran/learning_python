27d3c2e3df5acef24716e83e3f18e6f30f97bef9
Hammad <105131616+alee-codes@users.noreply.github.com>
2022-09-03 11:29:12 +0500

    Update factorial_recursion.py

--- factorial_recursion.py
+++ factorial_recursion.py
@@ -1,9 +1,16 @@
 def factorial(num):
     if num == 0 or num == 1:
         return 1
+    elif num > 50:
+        print('Memeory error.')
     else:
         return num * factorial(num - 1)
 
 
 result = factorial(7)
 print(result)
+
+# if the input is greater than 50, the program prints line 5 statement i.e. Memeory error but it also prints out None. Can you please explain why? 
+# output
+# Memeory error.
+# None
