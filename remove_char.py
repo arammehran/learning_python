fca918e73107ce501488c8bd02a05e7f0bc37714
Hammad <105131616+alee-codes@users.noreply.github.com>
2022-09-03 21:14:46 +0500

    Update remove_char.py

--- remove_char.py
+++ remove_char.py
@@ -1,5 +1,8 @@
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
 
+# https://pynative.com/python-basic-exercise-for-beginners/
+# Exercise 4. Please compare my solution with the one on the above link. I didn't use function. And I used a different method to solve it. i am not sure if it;s good or not. 
+
 
 word = input('Enter a word: \n')
 n = int(input("n is: \n"))
